34419589afe8cb45b3c486ed58413464cb6518fa
MuhamadMwaas <mwaas@proton.me>
2026-06-03 23:46:03 +0300

    fix

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -50,6 +50,8 @@ class AdminPanelProvider extends PanelProvider {
             ->default()
             ->id('admin')
             ->path('admin')
+            ->brandLogo('/image/logo.png')
+            ->favicon('/image/logo.png')
             ->login(\App\Filament\Pages\Auth\Login::class)
             ->colors([
                 'primary' => Color::Amber,
